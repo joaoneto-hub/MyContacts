feda81433515495b45231b7068d5c8292b45a92e
joaoneto-hub <joaorodrigues8182@gmail.com>
2022-08-20 16:13:25 -0300

    Criando Componentes de selec e input

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,3 +1,20 @@
+import PageHeader from '../../components/PageHeader';
+
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
 export default function NewContact() {
-  return 'NewContact';
+  return (
+    <>
+      <PageHeader
+        title="Novo Contato"
+      />
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
+  );
 }
